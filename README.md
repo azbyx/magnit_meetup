efcd50f2218f11378d20725be78bd814a3338068
azbyx <49538101+azbyx@users.noreply.github.com>
2021-11-27 23:28:50 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+
+## Task for Magnit Tech++ MeetUp 29.11.2021
+
+Задача Magnit Tech++ MeetUp
+Определена функция с сигнатурой: 
+
+void do_something(bool a, int b, std::string_view c) 
+
+Определить функцию, принимающую в произвольном порядке аргументы типов bool, int, std::string_view и вызывающую функцию do_something с переданными параметрами в качестве аргументов.
---
+++ sources/magicfunction.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <iostream>
+#include <tuple>
+namespace impl {
+  using TypeTpl = std::tuple<bool, int, std::string_view>;
+
+  template<typename Arg>
+  void magicImpl(TypeTpl& tpl, Arg arg) {
+    if constexpr (std::is_same_v<Arg, bool>)
+      std::get<0>(tpl) = arg;
+    else if constexpr  (std::is_same_v<Arg, int>)
+      std::get<1>(tpl) = arg;
+    else if constexpr  (std::is_same_v<Arg, std::string_view>)
+      std::get<2>(tpl) = arg;
+  }
+
+  template<typename Arg, typename... Tail>
+  void magicImpl(TypeTpl& tpl, Arg arg, Tail ...tail) {
+    if constexpr (std::is_same_v<Arg, bool>)
+      std::get<0>(tpl) = arg;
+    else if constexpr  (std::is_same_v<Arg, int>)
+      std::get<1>(tpl) = arg;
+    else if constexpr  (std::is_same_v<Arg, std::string_view>)
+      std::get<2>(tpl) = arg;
+    magicImpl(tpl, tail...);
+  }
+} // namespace impl
+
+void do_something(bool a, int b, std::string_view c) {
+  std::cout << "boolVar = " << a
+            << ", intVar = " << b
+            << ", strviewVar = " << c << std::endl;
+}
+
+template<typename... Args, typename F = std::enable_if_t<sizeof...(Args) == 3>>
+void magicFunction(Args ...args) {
+  std::tuple<bool, int, std::string_view> res;
+  impl::magicImpl(res, args...);
+  auto [boolVar, intVar, strviewVar] = res;
+  do_something(boolVar, intVar, strviewVar);
+
+}
